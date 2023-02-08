39ea067c1dbcd54bf2ec5ebc686293a885be1385
Kuppusamymayakrishnan <ks759502@gmail.com>
2023-02-08 20:25:04 +0530

    code is done

---
+++ src/com/demo/Sampleclass.java
@@ -0,0 +1,14 @@
+package com.demo;
+
+public class Sampleclass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("clone demo");
+		
+		System.out.println("123000");
+		
+		
+	}
+	
+}
